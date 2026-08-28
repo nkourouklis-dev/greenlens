12ece2caae883f41e2b7e96852530b8f3c2bf78c
Nikolaos Kourouklis <nkourouklis@kafkas.gr>
2026-08-28 12:54:29 +0300

    Add barcode scanner

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Scan from "./pages/Scan";
+import AddProduct from "./pages/AddProduct";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/scan" element={<Scan />} />
+        <Route path="/add-product" element={<AddProduct />} />
       </Routes>
     </BrowserRouter>
   );
